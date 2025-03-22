283efa6fb7782aabe72da781cac38fbf5e278c27
Rodrigo Xavier Vargas <rxvargas.92@gmail.com>
2025-03-22 11:54:23 -0300

    chore: configuracion inicil del proyecto

---
+++ griffindor-backend/src/main/java/com/devathon/griffindor_backend/GriffindorBackendApplication.java
@@ -0,0 +1,13 @@
+package com.devathon.griffindor_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GriffindorBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GriffindorBackendApplication.class, args);
+	}
+
+}
