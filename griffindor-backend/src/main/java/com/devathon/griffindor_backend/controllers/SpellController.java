72f2251c0de2c1ea41740d8b8ceb6bcc0f7160c4
AbrilSabatini <abrilsabatini2004@gmail.com>
2025-03-31 19:09:59 -0300

    feat(spell): add http controller for spells

---
+++ griffindor-backend/src/main/java/com/devathon/griffindor_backend/controllers/SpellController.java
@@ -0,0 +1,32 @@
+package com.devathon.griffindor_backend.controllers;
+
+import com.devathon.griffindor_backend.dtos.SpellResponseDto;
+import com.devathon.griffindor_backend.models.Spell;
+import com.devathon.griffindor_backend.services.SpellService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Controller
+@RequestMapping("spells")
+public class SpellController {
+
+    private final SpellService spellService;
+
+    public SpellController(SpellService spellService) {
+        this.spellService = spellService;
+    }
+
+    @GetMapping("")
+    public ResponseEntity<List<SpellResponseDto>> getAllSpell(){
+        // Mappear cada item de la lista a dto
+        List<SpellResponseDto> spells = spellService.getAll().stream()
+                .map(SpellResponseDto::new)
+                .collect(Collectors.toList());
+        return ResponseEntity.ok(spells);
+    }
+}
