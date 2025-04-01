86075d3a5d0c83237bfe37be60bc34ecb8eaf38a
entrenadev <manuel.entrena@gmail.com>
2025-04-01 23:21:32 +0200

    feat(socket): :sparkles: Availabled async and schedule

--- griffindor-backend/src/main/java/com/devathon/griffindor_backend/GriffindorBackendApplication.java
+++ griffindor-backend/src/main/java/com/devathon/griffindor_backend/GriffindorBackendApplication.java
@@ -2,8 +2,12 @@ package com.devathon.griffindor_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableAsync
+@EnableScheduling
 public class GriffindorBackendApplication {
 
 	public static void main(String[] args) {
